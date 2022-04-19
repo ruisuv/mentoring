5e30ebf9dc2ec0054a9be52421a0116c4d191691
Arturo Vargas <ruisu.vargas@gmail.com>
2022-04-19 11:55:06 -0500

    Use static import for HTTP Status

--- src/main/java/com/nearsoft/mentoring/controller/BookController.java
+++ src/main/java/com/nearsoft/mentoring/controller/BookController.java
@@ -4,12 +4,14 @@ package com.nearsoft.mentoring.controller;
 import com.nearsoft.mentoring.model.Book;
 import com.nearsoft.mentoring.tree.Tree;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 @RequestMapping("/api/books")
@@ -25,7 +27,7 @@ public class BookController {
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Tree<Book>> getAll() {
         Tree<Book> response = service.getAll();
-        return response != null ? new ResponseEntity<>(response, HttpStatus.OK) : new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        return response != null ? new ResponseEntity<>(response, OK) : new ResponseEntity<>(NOT_FOUND);
     }
 
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
